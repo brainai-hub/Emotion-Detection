afef2b623736202597f042ba2d08734e80ab23e6
Brain AI <73767162+brainai-hub@users.noreply.github.com>
2024-09-22 13:49:33 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,31 +4,51 @@ import cv2
 import numpy
 import utils
 import io
-
+import tempfile
+import moviepy.editor as mpy
+from camera_input_live import camera_input_live
+ 
 st.set_page_config(
-    page_title="Age/Gender/Emotion",
+    page_title="Emotion Detection",
     page_icon=":sun_with_face:",
     layout="centered",
     initial_sidebar_state="expanded",)
 
-st.title("Age/Gender/Emotion :sun_with_face:")
+st.title("Emotion Detection :sun_with_face:")
 
 def play_video(video_source):
     camera = cv2.VideoCapture(video_source)
+    fps = camera.get(cv2.CAP_PROP_FPS)
+    temp_file_2 = tempfile.NamedTemporaryFile(delete=False,suffix='.mp4')
+    video_row=[]
 
+    # frame
+    total_frames = int(camera.get(cv2.CAP_PROP_FRAME_COUNT))
+    progress_bar = st.progress(0)
+    frame_count = 0
+    
     st_frame = st.empty()
+    
     while(camera.isOpened()):
         ret, frame = camera.read()
-
         if ret:
-            visualized_image = utils.predict_image(frame, conf_threshold)
+            try:
+                visualized_image = utils.predict_image(frame, conf_threshold)
+            except:
+                visualized_image = frame
             st_frame.image(visualized_image, channels = "BGR")
-
+            video_row.append(cv2.cvtColor(visualized_image,cv2.COLOR_BGR2RGB))
+            frame_count +=1 
+            progress_bar.progress(frame_count/total_frames, text=None)
         else:
+            progress_bar.empty()
             camera.release()
- 
+            st_frame.empty()
             break
 
+    clip = mpy.ImageSequenceClip(video_row, fps = fps)
+    clip.write_videofile(temp_file_2.name)
+    st.video(temp_file_2.name)
 
 st.sidebar.header("Type")
 source_radio = st.sidebar.radio("Select Source", ["IMAGE", "VIDEO", "WEBCAM"])
@@ -45,76 +65,32 @@ if source_radio == "IMAGE":
         uploaded_image = PIL.Image.open(input)
         uploaded_image_cv = cv2.cvtColor(numpy.array(uploaded_image), cv2.COLOR_RGB2BGR)
         visualized_image = utils.predict_image(uploaded_image_cv, conf_threshold = conf_threshold)
-  
         st.image(visualized_image, channels = "BGR")
+    else: 
+        st.image("assets/sample_image.jpg")
+        st.write("Click on 'Browse Files' in the sidebar to run inference on an image." )
 
 temporary_location = None
 if source_radio == "VIDEO":
     st.sidebar.header("Upload")
     input = st.sidebar.file_uploader("Choose an video.", type=("mp4"))
-    
+  
     if input is not None:
-
         g = io.BytesIO(input.read())
         temporary_location = "upload.mp4" 
-
         with open(temporary_location, "wb") as out: 
             out.write(g.read())
-
         out.close() 
 
     if temporary_location is not None:
-
         play_video(temporary_location)
-        if st.button("Replay", type="primary"):
-            pass
-
+    else:
+        st.video("assets/sample_video.mp4")
+        st.write("Click on 'Browse Files' in the sidebar to run inference on an video." )
+       
 if source_radio == "WEBCAM":
-    play_video(0) 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    image = camera_input_live()
+    uploaded_image = PIL.Image.open(image)
+    uploaded_image_cv = cv2.cvtColor(numpy.array(uploaded_image), cv2.COLOR_RGB2BGR)
+    visualized_image = utils.predict_image(uploaded_image_cv, conf_threshold = conf_threshold)
+    st.image(visualized_image, channels = "BGR")
