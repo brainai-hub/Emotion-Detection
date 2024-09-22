ac7df71289c5158cde6daf884ea0e9895453bd49
Brain AI <73767162+brainai-hub@users.noreply.github.com>
2024-09-22 13:51:37 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -7,7 +7,6 @@ core = ov.Core()
 
 model_face = core.read_model(model='models/face-detection-adas-0001.xml')
 compiled_model_face = core.compile_model(model = model_face, device_name="CPU")
-
 input_layer_face = compiled_model_face.input(0)
 output_layer_face = compiled_model_face.output(0)
 
@@ -25,48 +24,34 @@ output_layer_ag = compiled_model_ag.output
 
 def preprocess(image, input_layer):
     N, input_channels, input_height, input_width = input_layer.shape
-
     resized_image = cv2.resize(image, (input_width, input_height))
     transposed_image = resized_image.transpose(2, 0, 1)
     input_image = np.expand_dims(transposed_image, 0)
-
     return input_image 
 
 def find_faceboxes(image, results, confidence_threshold):
     results = results.squeeze()
-
     scores = results[:,2]
     boxes = results[:, -4:]
-
     face_boxes = boxes[scores >= confidence_threshold]
     scores = scores[scores >= confidence_threshold]
-
     image_h, image_w, image_channels = image.shape 
- 
     face_boxes = face_boxes*np.array([image_w, image_h, image_w, image_h])
     face_boxes = face_boxes.astype(np.int64)
-
     return face_boxes, scores
 
 def draw_age_gender_emotion(face_boxes, image):
-    
     EMOTION_NAMES =  ['neutral', 'happy', 'sad', 'surprise', 'anger']
-    
     show_image = image.copy()
-   
     for i in range(len(face_boxes)):
-
         xmin, ymin, xmax, ymax = face_boxes[i]
         face = image[ymin:ymax, xmin:xmax]
-        
         # --- emotion ---
         input_image = preprocess(face, input_layer_emo)
         results_emo = compiled_model_emo([input_image])[output_layer_emo]
- 
         results_emo = results_emo.squeeze()
         index = np.argmax(results_emo)       
         # --- emotion ---
-        
         
         # --- age and gender ---
         input_image_ag = preprocess(face, input_layer_ag)
@@ -79,12 +64,10 @@ def draw_age_gender_emotion(face_boxes, image):
         
         if (gender[0]>=0.65):
             gender = "female"
-            box_color = (200, 200, 0)
-            
+            box_color = (200, 200, 0)     
         elif (gender[1]>=0.55):
             gender = "male"
             box_color = (0, 200, 200)
-        
         else:
             gender = "unknown"
             box_color = (200, 200, 200)
@@ -104,61 +87,4 @@ def predict_image(image, conf_threshold):
     results = compiled_model_face([input_image])[output_layer_face]
     face_boxes, scores = find_faceboxes(image, results, conf_threshold)
     visualize_image = draw_age_gender_emotion(face_boxes, image)
-
     return visualize_image
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
